933fc0e5f2f3eed1fc81ef60a0c2755d7fd414ca
Anthony O'Brien <asobrien@jwplayer.com>
2016-07-22 21:19:42 -0400

    Fixes issues with missing config files, and cli args

--- main.go
+++ main.go
@@ -14,18 +14,52 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
+	"reflect"
 	"strconv"
 	"time"
 )
 
 var (
-	Version string = "0.1.0"
-	config  = viper.New()
+	Version string = "0.1.1"
+	config         = viper.New()
 	Trace   *log.Logger
 	Info    *log.Logger
 	Warning *log.Logger
 	Error   *log.Logger
 )
+
+var cliParam = new(cliFlag)
+
+// cliFlag allows for cli <--> viper conversion
+type cliFlag struct {
+	Port        int
+	port        int
+	token       string
+	secret      string
+	logfile     string
+	path        string
+	healthcheck string
+	debug       bool
+}
+
+// getField returns clfFlag field value via string
+// this allows for cli --> viper conversion in a loop
+func (c *cliFlag) getField(field string) interface{} {
+	r := reflect.ValueOf(c)
+	f := reflect.Indirect(r).FieldByName(field)
+
+	switch f.Type().Name() {
+	default:
+		panic(fmt.Errorf("Unknown type: %T\n", f))
+	case "int":
+		return f.Int()
+	case "string":
+		return f.String()
+	case "bool":
+		return f.Bool()
+	}
+	return nil
+}
 
 // PullRequest Event Handler
 type PullRequest struct {
@@ -99,7 +133,7 @@ func initLoggers(debug bool) error {
 	if config.IsSet("logfile") && config.GetString("logfile") != "" {
 		logFile, err = os.OpenFile(config.GetString("logfile"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0640)
 		if err != nil {
-			// No logger is setup yet, so write to Stderr
+			// no logger is setup yet, so write to Stderr
 			fmt.Fprintln(os.Stderr, "Failed to open log file: ", err)
 			os.Exit(1)
 		}
@@ -206,36 +240,31 @@ func serve() {
 }
 
 func runApp(c *cli.Context) error {
-	// Set search paths
+
+	// Set file name & search paths
 	config.SetConfigName("config")
 	config.AddConfigPath("/etc/nomaster/")
 	config.AddConfigPath("$HOME/.nomaster")
 
-	// Exit if config has errors
+	// Exit if config has errors, outside of file not found
 	err := config.ReadInConfig()
-	if err != nil {
-		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	if err != nil && err != viper.UnsupportedConfigError("") {
+		fmt.Fprintln(os.Stderr, "Error reading configuration file: ", err)
+		os.Exit(1)
 	}
 
 	// Set cli flags if not set and viper.IsSet(flag)
 	// this gets rid of globals and we just pass around cli Context
 	// Set cli overrides & defaults into viper
 	for _, flag := range c.GlobalFlagNames() {
-		if c.IsSet(flag) || !config.IsSet(flag) {
-			if !config.IsSet(flag) {
-				config.SetDefault(flag, c.Generic(flag))
-			} else {
-				config.Set(flag, c.Generic(flag))
-			}
+		if !config.IsSet(flag) {
+			config.Set(flag, cliParam.getField(flag)) // set default (cli option or default)
+		}
+		if c.IsSet(flag) {
+			config.Set(flag, cliParam.getField(flag)) // explicitly set option (cli flag given)
 		}
 	}
 
-	// Explicitly override debug value from cli
-	if c.IsSet("debug") {
-		var d interface{} = c.Bool("debug")
-		config.Set("debug", d)
-	}
-	// initialize loggers
 	initLoggers(config.GetBool("debug"))
 
 	if config.ConfigFileUsed() != "" {
@@ -273,46 +302,51 @@ func main() {
 	app.Flags = []cli.Flag{
 		// port flag
 		cli.IntFlag{
-			Name:  "port, p",
-			Value: 8888,
-			Usage: "sever `PORT`",
+			Name:        "port, p",
+			Value:       8888,
+			Usage:       "sever `PORT`",
+			Destination: &cliParam.port,
 		},
 		// oauth token flag
 		cli.StringFlag{
-			Name:  "token, t",
-			Value: "",
-			Usage: "Github OAuth `TOKEN`",
+			Name:        "token, t",
+			Value:       "",
+			Usage:       "Github OAuth `TOKEN`",
+			Destination: &cliParam.token,
 		},
 		// webhook secret signing key
 		cli.StringFlag{
-			Name:  "secret, s",
-			Value: "",
-			Usage: "Signing `SECRET` key",
+			Name:        "secret, s",
+			Value:       "",
+			Usage:       "Signing `SECRET` key",
+			Destination: &cliParam.secret,
 		},
 		// logfile location
 		cli.StringFlag{
-			Name:  "logfile, l",
-			Usage: "Path to log `FILE`",
+			Name:        "logfile, l",
+			Usage:       "Path to log `FILE`",
+			Destination: &cliParam.logfile,
 		},
 		// hookserve endpoint
 		cli.StringFlag{
-			Name:  "path, e",
-			Value: "/",
-			Usage: "Webhook application `ENDPOINT`",
+			Name:        "path, e",
+			Value:       "/",
+			Usage:       "Webhook application `ENDPOINT`",
+			Destination: &cliParam.path,
 		},
 		// hookserve endpoint
 		cli.StringFlag{
-			Name:  "healthcheck, c",
-			Value: "/ping",
-			Usage: "Healthcheck `ENDPOINT`",
+			Name:        "healthcheck, c",
+			Value:       "/ping",
+			Usage:       "Healthcheck `ENDPOINT`",
+			Destination: &cliParam.healthcheck,
 		},
 		// debug mode
 		cli.BoolFlag{
-			Name:  "debug, d",
-			Usage: "Enable debug mode",
+			Name:        "debug, d",
+			Usage:       "Enable debug mode",
+			Destination: &cliParam.debug,
 		},
 	}
-
 	app.Run(os.Args)
-
 }
